c32efb715ec0c0cc03787c590532e5171df5dffa
Vadim Golubev <golubevvadik2013@yandex.ru>
2021-08-28 17:21:27 +0300

    Добавлены ассоциации

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,10 @@
 class Test < ApplicationRecord
 
-  has_and_belongs_to_many :users
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  has_many :questions
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   def self.find_by_category(category)
     Test.joins("JOIN categories ON tests.category_id = categories.id")
