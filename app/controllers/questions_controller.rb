44da34a33c814b9f9ff97d500f8edb5dcd0cc5d7
Vadim Golubev <golubevvadik2013@yandex.ru>
2021-09-05 23:29:21 +0300

    routes and controllers 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test
+  before_action :find_test, only: [:index, :create, :new]
+  before_action :find_question, only: [:edit, :show, :update, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -12,23 +13,27 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    render inline: '<h1>Test: <%= @test.title %></h1>
-                    <h3>Question №<%= params[:id].to_i %>:</h3>
-                    <p><%= Question.find(params[:id]).body %></p>'
+    render inline: '<h1>Test: <%= @question.test.title %></h1>
+                    <h3>Question:</h3>
+                    <p><%= @question.body %></p>'
   end
 
   def new
-
+    @question = Question.new
   end
 
   def create
-    question = @test.questions.create(question_params)
+    question = @test.questions.new(question_params)
 
-    render inline: '<p>Question "<%= params[:body] %>" successfully created</p>'
+    if question.save
+      render inline: '<p>Question "<%= params[:question][:body] %>" successfully created</p>'
+    else
+      render html: '<p>Something went wrong...</p>'
+    end
   end
 
   def destroy
-    Question.find(params[:id]).destroy_all
+    @question.destroy
     redirect_to test_questions_path
   end
 
@@ -40,6 +45,10 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
