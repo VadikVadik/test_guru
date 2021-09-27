a87324e5804b69b7b71ec345e1ec55a780252b28
Vadim Golubev <golubevvadik2013@yandex.ru>
2021-09-27 11:51:34 +0300

    javascript2

--- app/javascript/utilities/password-confirmation.js
+++ app/javascript/utilities/password-confirmation.js
@@ -1,6 +1,5 @@
 document.addEventListener('turbolinks:load', function() {
   var confirmationField = document.querySelector('.pass-confirm')
-
   if (confirmationField) { confirmationField.addEventListener('input', matchNotification) }
 })
 
@@ -9,18 +8,16 @@ function matchNotification() {
   var match = document.querySelector('.text-success')
   var notMatch = document.querySelector('.text-danger')
 
-  if (password.value != "") {
-    if (password.value == this.value) {
-      notMatch.classList.add('hide')
-      match.classList.remove('hide')
-    } else if (this.value == "") {
-      match.classList.add('hide')
-      notMatch.classList.add('hide')
-    } else {
-      match.classList.add('hide')
-      notMatch.classList.remove('hide')
-    }
+  if (password.value == "") { return }
+
+  if (password.value == this.value) {
+    notMatch.classList.add('hide')
+    match.classList.remove('hide')
+  } else if (this.value == "") {
+    match.classList.add('hide')
+    notMatch.classList.add('hide')
   } else {
-    return
+    match.classList.add('hide')
+    notMatch.classList.remove('hide')
   }
 }
