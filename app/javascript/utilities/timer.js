aa8515b2e979a0d4a9b12e56969aedfaad23257e
Vadim Golubev <golubevvadik2013@yandex.ru>
2021-10-12 11:54:20 +0300

    timer2

--- app/javascript/utilities/timer.js
+++ app/javascript/utilities/timer.js
@@ -2,27 +2,31 @@ document.addEventListener('turbolinks:load', function () {
   var timer = document.querySelector('.timer')
 
    if (timer) {
+     timer.innerHTML = Math.floor(timeDiff(timer)/60) + ":" + Math.floor(timeDiff(timer)%60)
      setInterval(showCountdown, 1000)
    }
 })
 
 function showCountdown() {
   var timer = document.querySelector('.timer')
+  var minutes = Math.floor(timeDiff(timer)/60)
+  var secundes = Math.floor(timeDiff(timer)%60)
 
+  timer.innerHTML = addZero(minutes) + ":" + addZero(secundes)
+}
+
+function timeDiff(timer) {
   var nowDate = new Date()
-
   var timeNow = nowDate.getTime() / 1000
   var timeOut = timer.dataset.timeOut
 
-  var timeDiff = timeOut - timeNow
+  return timeOut - timeNow
+}
 
-  var days = timeDiff / (60 * 60 * 24)
-  timeDiff = timeDiff % (60 * 60 * 24)
-  var hours = timeDiff / (60 * 60)
-  timeDiff = timeDiff % (60 * 60)
-  var minutes = timeDiff / 60
-  var secundes = timeDiff % 60
-
-  timer.innerHTML = Math.floor(minutes) + ":" + Math.floor(secundes)
-
+function addZero(num) {
+  var res = num
+  if (num < 10) {
+    res = "0" + num
+  }
+  return res
 }
